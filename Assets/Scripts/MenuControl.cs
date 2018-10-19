bfdc444126ed495346bd0d7fe0747950f3561bb1
Amanda Abedmamoore <abedm002@empress.csusm.edu>
2018-10-19 16:17:28 -0700

    First commit

---
+++ Assets/Scripts/MenuControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour {
+
+	public void PlayButton(string newLevel){
+		SceneManager.LoadScene(newLevel);
+	}
+	
+	public void QuitButton(){
+		Application.Quit();
+	}
+}
