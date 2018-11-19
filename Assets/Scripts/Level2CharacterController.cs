2bc4cb2c28eb89d214feaacb82585c053370c4f9
Amanda Abedmamoore <abedm002@empress.csusm.edu>
2018-11-18 17:05:06 -0800

    Added collectible and text

---
+++ Assets/Scripts/Level2CharacterController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Level2CharacterController: MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    public float speed;
+
+    public Text win;
+    public Text countdownText;
+    private Vector3 movement;
+    private int countdown;
+
+    public GameObject shark;
+
+    void Start()
+    {
+        shark = GameObject.FindGameObjectWithTag("shark");
+        rb = GetComponent<Rigidbody>();
+        countdown = 5;
+        //speed = 10.0f;
+        SetCount();
+        win.text = "";
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            SceneManager.LoadScene("MainMenu");
+        }
+        if(countdown == 0){
+            Destroy(shark);
+        }
+    }
+
+    void FixedUpdate()
+    {  
+        movement = Vector3.zero;
+     	movement.x = Input.GetAxis("Horizontal");
+     	movement.z = Input.GetAxis("Vertical");
+        movement.y = 0;
+        movement = movement.normalized;
+
+		if(Input.GetKey(KeyCode.UpArrow)){
+			transform.LookAt(movement + transform.position);
+			transform.eulerAngles = new Vector3(0, 270, 0); 
+	    	transform.Translate(movement * speed * Time.deltaTime, Space.World);
+		}
+        
+        if(Input.GetKey(KeyCode.DownArrow)){
+        	transform.LookAt(movement + transform.position);
+        	transform.eulerAngles = new Vector3(0, -270, 0); 
+        	transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        }
+        
+        if(Input.GetKey(KeyCode.LeftArrow)){
+        	transform.LookAt(movement + transform.position);
+        	transform.eulerAngles = new Vector3(0, 180, 0); 
+        	transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        }
+        
+        if(Input.GetKey(KeyCode.RightArrow)){
+        	transform.LookAt(movement + transform.position);
+        	transform.eulerAngles = new Vector3(0, 0, 0); 
+        	transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        }
+       /* 
+        if (transform.position.y != 1)
+        {
+            transform.position = new Vector3(transform.position.x, 1, transform.position.z);
+        }
+        */
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            countdown -= 1;
+            SetCount();
+        }
+    }
+
+    void SetWinText(){
+        win.text = "Level Complete!";
+        countdownText.text = "";
+    }  
+
+    void SetCount(){
+        countdownText.text = "Fish Eggs Left: " + countdown.ToString();
+        if(countdown == 0){
+            SetWinText();
+        }
+    }
+}
