8ab966c2b836acb7a5efc8fde637861a398ae655
Amanda Abedmamoore <abedm002@empress.csusm.edu>
2018-11-30 16:17:27 -0800

    Added instructions on main menu how to play button

--- Assets/Scripts/MenuControl.cs
+++ Assets/Scripts/MenuControl.cs
@@ -5,11 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MenuControl : MonoBehaviour {
 
+	public Transform mainMenu, optionsMenu;
+
 	public void PlayButton(string newLevel){
 		SceneManager.LoadScene(newLevel);
 	}
 	
 	public void QuitButton(){
 		Application.Quit();
+	}
+
+	public void HowToPlayButton(bool clicked){
+		if(clicked == true){
+				optionsMenu.gameObject.SetActive(clicked);
+				mainMenu.gameObject.SetActive(false);
+			}else{
+				optionsMenu.gameObject.SetActive(clicked);
+				mainMenu.gameObject.SetActive(true);
+			}
 	}
 }
